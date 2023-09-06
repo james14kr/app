41edadd0f176852dcc9293855304a2959910b3c3
hwangminseo <hwangms14kr@naver.com>
2023-09-06 16:32:27 +0900

    AndroidHybridApp

---
+++ app/src/main/java/com/example/loading/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.loading;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, LoadingActivity.class);
+        startActivity(intent);
+    }
+}
